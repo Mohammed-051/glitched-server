eff003c1afaf21859c8a5471a775b45632d0262d
Mohammed-051 <Mohammed-051@users.noreply.github.com>
2026-04-17 20:49:38 +0600

    Initial project scaffolding

---
+++ main.cpp
@@ -0,0 +1,29 @@
+﻿#include <GL/glut.h>
+
+void initializeScene() {
+    glClearColor(0.04f, 0.04f, 0.04f, 1.0f);
+    glEnable(GL_DEPTH_TEST);
+}
+
+void renderFrame() {
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+
+    glutSwapBuffers();
+}
+
+int main(int argc, char** argv) {
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+    glutInitWindowSize(960, 640);
+    glutInitWindowPosition(120, 80);
+    glutCreateWindow("Replace Escape Room - CG Project");
+
+    initializeScene();
+    glutDisplayFunc(renderFrame);
+    glutMainLoop();
+
+    return 0;
+}
